d6cf565deb7d4438276075fe928aef79a480df3e
Rodrigo Labarile <labarilerodrigo@gmail.com>
2025-08-08 17:18:40 -0300

    Include src directory in Tailwind CSS content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     darkMode: 'class', // ⬅️ Add this line
-    content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+        "./src/**/*.{js,ts,jsx,tsx}",
+    ],
     theme: {
       extend: {
         animation: {
